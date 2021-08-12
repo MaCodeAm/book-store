5364f4e32a9b5e085d957ec026d8eb87069a1dc6
Mariam Ghazaryan <mariamghazaryan114@gmail.com>
2021-08-12 21:26:48 +0400

    Initial commit

---
+++ src/main/java/project/Start/HomeController.java
@@ -0,0 +1,16 @@
+package project.Start;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/")
+    public String print() {
+        return "Hello world";
+    }
+}
