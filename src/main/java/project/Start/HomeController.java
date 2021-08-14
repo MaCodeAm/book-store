c4bc95d422d0fb69ae1a1b18a2932a7a0c28224e
Mariam Ghazaryan <mariamghazaryan114@gmail.com>
2021-08-14 17:35:04 +0400

    N2

--- src/main/java/project/Start/HomeController.java
+++ src/main/java/project/Start/HomeController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping("/")
     public String print() {
-        return "Hello world";
+        return "index.html";
     }
 }
